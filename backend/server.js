2622a11233ec334b748ad5f4eb2abb9e6c4bfbaa
Aakarsh Divyam <divyamaakarsh22@gmail.com>
2025-10-16 18:31:16 +0530

    Update CORS origin to use www subdomain

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://aakarsh.me'
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://www.aakarsh.me'
 }));
 
 const API_KEY = process.env.TMDB_API_KEY;
